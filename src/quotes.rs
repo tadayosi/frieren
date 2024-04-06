a2fa2763bd983ae76560b4a38a7132162dae70a5
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-04-06 17:31:41 +0900

    initial frieren impl

---
+++ src/quotes.rs
@@ -0,0 +1,5 @@
+pub const QUOTES: [&str; 3] = [
+    "We only traveled together for a mere ten years...",
+    "Yes, Himmel would say so.",
+    "You are in front of a mage who has lived for more than one thousand years.",
+];
